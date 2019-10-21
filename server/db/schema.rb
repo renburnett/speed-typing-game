0bba4c25f7eac04b3461319cd6992a4fe698e802
Krista Loven <krista.loven.87@gmail.com>
2019-10-21 12:30:09 -0700

    dropped accidental table

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_192051) do
+ActiveRecord::Schema.define(version: 2019_10_21_192750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,13 +33,6 @@ ActiveRecord::Schema.define(version: 2019_10_21_192051) do
   end
 
   create_table "word_banks", force: :cascade do |t|
-    t.string "word"
-    t.integer "length"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "word_lists", force: :cascade do |t|
     t.string "word"
     t.integer "length"
     t.datetime "created_at", precision: 6, null: false
