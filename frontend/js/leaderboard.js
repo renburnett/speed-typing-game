6d83ea3a244d8a5443d2b89651cda862ece95756
Krista Loven <krista.loven.87@gmail.com>
2019-10-23 17:46:47 -0700

    added WPM logic, changed words seen adn words typed to numerical vals

--- frontend/js/leaderboard.js
+++ frontend/js/leaderboard.js
@@ -15,6 +15,7 @@ class Leaderboard {
     this.createAccountAndBestRunElements = this.createAccountAndBestRunElements.bind(this);
     this.fetchUsers = this.fetchUsers.bind(this);
     this.deleteRun = this.deleteRun.bind(this);
+    this.calcWpm = this.calcWpm.bind(this);
     this.handleLeaderboardToggle = this.handleLeaderboardToggle.bind(this);
   }
 
@@ -34,10 +35,10 @@ class Leaderboard {
     };
 
     fetch(`http://localhost:3000/runs/${runId}`, config)
-    .then(() => {
-      this.clearUserAccountsFromTable();
-      this.fetchUsers();
-    });
+      .then(() => {
+        this.clearUserAccountsFromTable();
+        this.fetchUsers();
+      });
   }
 
   clearUserAccountsFromTable () {
@@ -49,7 +50,7 @@ class Leaderboard {
   displayAccountsAndBestRuns (accounts) {
     const runElements = [];
     this.clearUserAccountsFromTable();
-    
+
     for (const account of accounts) {
       runElements.push(this.createAccountAndBestRunElements(account));
     }
@@ -66,12 +67,7 @@ class Leaderboard {
 
   orderUsersBestRuns (arr) {
     arr.sort((a, b) => {
-      if (a.score > b.score) 
-        return -1;
-      else if (a.score < b.score)
-        return 1;
-      else
-        return 0;
+      if (a.score > b.score) { return -1; } else if (a.score < b.score) { return 1; } else { return 0; }
     });
   }
 
@@ -96,39 +92,53 @@ class Leaderboard {
     return bestRun;
   }
 
+  calcWpm (lengthTyped, bestRun) {
+    // compare create and update times of the run
+    // return Math.round(minutesInRun / lengthTyped);
+    const startTime = new Date(bestRun.created_at);
+    const endTime = new Date(bestRun.updated_at);
+    const minutesInRun = ((endTime - startTime) / 60000); // how many mintutes in run
+    return Math.round(lengthTyped / minutesInRun * 100) / 100;
+  }
+
   createAccountAndBestRunElements (account) {
     const runContainer = document.createElement('tr');
     const userName = document.createElement('td');
     const runScore = document.createElement('td');
     const runWordsTyped = document.createElement('td');
     const runWordsSeen = document.createElement('td');
+    const runWPM = document.createElement('td');
     const runDeleteButton = document.createElement('button');
 
     const bestRun = this.getUserBestRun(account.runs);
 
     runDeleteButton.addEventListener('click', () => {
-      if (bestRun.id !== undefined){
+      if (bestRun.id !== undefined) {
         this.deleteRun(bestRun.id);
       } else {
         console.log('Cannot delete null run.');
-        //TODO: maybe change to modal popup?
+        // TODO: maybe change to modal popup?
       }
     });
 
+    const lengthTyped = bestRun.words_typed.split(',').length;
+    const lengthSeen = bestRun.words_seen.split(',').length;
+
     userName.textContent = account.username;
     runScore.textContent = bestRun.score;
-    runWordsTyped.textContent = bestRun.words_typed;
-    runWordsSeen.textContent = bestRun.words_seen;
+    runWordsTyped.textContent = lengthTyped;
+    runWordsSeen.textContent = lengthSeen;
+    runWPM.textContent = this.calcWpm(lengthTyped, bestRun);
     runDeleteButton.textContent = 'Delete';
-    runDeleteButton.classList.add("btn", "btn-primary", "link");
+    runDeleteButton.classList.add('btn', 'btn-primary', 'link');
 
-    runContainer.append(userName, runScore, runWordsTyped, runWordsSeen);
+    runContainer.append(userName, runScore, runWordsTyped, runWordsSeen, runWPM);
 
     if (game.account && Number(game.account.id) === Number(account.id)) {
       runContainer.append(runDeleteButton);
     }
 
-    return {runElements: runContainer, accountId: account.id, score: bestRun.score};
+    return { runElements: runContainer, accountId: account.id, score: bestRun.score };
   }
 
   handleLeaderboardToggle () {
@@ -141,4 +151,3 @@ class Leaderboard {
 }
 
 const leaderboard = new Leaderboard();
-
